bb696daf932086dde68cd69b8a553c4532c16012
nitin-codr <nitinporwal99@gmail.com>
2020-06-17 15:08:55 +0530

    destructure object for constructor of Dragon class and use getters for birthdate in default properties

--- dragon.js
+++ dragon.js
@@ -1,7 +1,14 @@
+const DEFALT_PROPERTIES = {
+    get birthDate() {
+        return new Date();
+    },
+    nickName: "unnamed"
+}
+
 class Dragon {
-    constructor (birthDate, nickName) {
-        this.birthDate=birthDate;
-        this.nickName=nickName;
+    constructor ({birthDate, nickName}={}) {
+        this.birthDate=birthDate || DEFALT_PROPERTIES.birthDate;
+        this.nickName=nickName || DEFALT_PROPERTIES.nickName;
     }
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const Dragon = require('./dragon');
 
-const fooey = new Dragon(new Date(), "fooey");
-const baloo = new Dragon(new Date(), "baloo");
 
-console.log('fooey', fooey);
+const baloo = new Dragon({
+    birthDate: new Date(),
+    nickName: "baloo"
+});
+
+const minar = new Dragon();
+
+setTimeout(() => {
+    const fooey = new Dragon({
+        birthDate: new Date(),
+        nickName: "fooey"
+    });
+    console.log('fooey', fooey);
+}, 3000);
+
+setTimeout(() => {
+    const gooby = new Dragon();
+    console.log('gooby', gooby);
+}, 3000);
+
 console.log('baloo', baloo);
+console.log('minar', minar);
