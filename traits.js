d8bb854bc0e6d4c90bfb22bc30de4d4483c4aafc
nitin-codr <nitinporwal99@gmail.com>
2020-06-17 19:50:40 +0530

    add traits for each type of dragons

---
+++ traits.js
@@ -0,0 +1,38 @@
+[
+    {
+        "type": "backgroundColor",
+        "values": [
+            "black",
+            "white",
+            "green",
+            "blue"
+        ]
+    },
+    {
+        "type": "pattern",
+        "values": [
+            "plain",
+            "striped",
+            "spotted",
+            "patcht"
+        ]
+    },
+    {
+        "type": "build",
+        "values": [
+            "slender",
+            "stocky",
+            "stockey",
+            "skinny"
+        ]
+    },
+    {
+        "type": "size",
+        "values": [
+            "small",
+            "medium",
+            "large",
+            "enormous"
+        ]
+    }
+]
