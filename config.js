e8c3b34f61f7a786b066c6284f865ed69c2895bd
nitin-codr <nitinporwal99@gmail.com>
2020-06-17 20:35:07 +0530

    add global constants in seperate config file

---
+++ config.js
@@ -0,0 +1,14 @@
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+const REFRESH_RATE = 5 // units
+
+module.exports ({
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY,
+    REFRESH_RATE
+})
