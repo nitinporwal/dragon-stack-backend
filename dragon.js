0de8fde8f63a1ce9900fa22ec587fd2ef7bdc7a6
nitin-codr <nitinporwal99@gmail.com>
2020-06-17 20:27:33 +0530

    link traits of each dragon with its properties

--- dragon.js
+++ dragon.js
@@ -1,14 +1,30 @@
+const TRAITS = require('./traits.json');
+
 const DEFALT_PROPERTIES = {
     get birthDate() {
         return new Date();
     },
-    nickName: "unnamed"
+    nickName: "unnamed",
+    get randomTraits() {
+        let traits=[];
+        TRAITS.map(TRAIT => {
+            let traitType = TRAIT.type;
+            let traitValues= TRAIT.values;
+
+            let traitValue = traitValues[
+                (Math.ceil(Math.random()*traitValues.length))%traitValues.length
+            ]
+            traits.push({traitType, traitValue}) 
+        })
+        return traits;
+    }
 }
 
 class Dragon {
-    constructor ({birthDate, nickName}={}) {
+    constructor ({birthDate, nickName, traits}={}) {
         this.birthDate=birthDate || DEFALT_PROPERTIES.birthDate;
         this.nickName=nickName || DEFALT_PROPERTIES.nickName;
+        this.traits=traits || DEFALT_PROPERTIES.randomTraits;
     }
 }
 
