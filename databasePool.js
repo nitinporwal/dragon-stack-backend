d600e248801977f86a852e0de32a51058f91aba7
nitin-codr <nitinporwal99@gmail.com>
2020-06-18 15:00:38 +0530

    add databasePool for pooling all the relations all together

---
+++ databasePool.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const databaseconfiguration = require('./secrets/databaseconfiguration');
+// const { response } = require('express');
+
+const pool = new Pool(databaseconfiguration);
+
+module.exports = pool;
+
+pool.query('SELECT * FROM generation').then(res => {
+    console.log('response.rows', res.rows);
+}).catch(err => {
+    return console.log('err', err);
+});
