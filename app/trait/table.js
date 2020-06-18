4dccd574290013007c5ad805ac5e4d334c147651
nitin-codr <nitinporwal99@gmail.com>
2020-06-18 19:15:09 +0530

    get traitId from trait table

---
+++ app/trait/table.js
@@ -0,0 +1,24 @@
+const pool = require("../../databasePool");
+
+class TraitTable {
+    static getTraitId = ({traitType, traitValue}) => {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `SELECT id FROM trait WHERE "traitType" = $1 AND "traitValue" = $2`,
+                [traitType, traitValue],
+                (error, response) => {
+                    if(error) console.log(error);
+
+                    resolve({ traitId: response.rows[0].id});
+                }
+            )
+        })
+    }
+}
+
+// TraitTable.getTraitId({ traitType: "backgroundColor", traitValue: "green"})
+//     .then(({ traitId }) => console.log('traitId', traitId))
+//     .catch(error => console.error('error', error)
+// )
+
+module.exports = TraitTable;
